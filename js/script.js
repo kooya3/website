bdfdd308d0d6c464d0ad326972dfa069c215a019
kooya3 <elyeestatua@gmail.com>
2023-09-11 20:27:05 +0300

    ops(@JS): Started on importing assets from the assets folder for dynamic calling

---
+++ js/script.js
@@ -0,0 +1,5 @@
+import check from '../assets/check.svg'
+import star from '../assets/star.svg'
+import sushi12 from '../assets/sushi-12.png'
+import sushi11 from '../assets/sushi-11.png'
+import sushi10 from '../assets/sushi-10.png'
