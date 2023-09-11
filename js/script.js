42d555ed5a1d965323b059ebd09b506c557def6d
kooya3 <elyeestatua@gmail.com>
2023-09-11 20:40:09 +0300

    ops(@JS): Declaring product cards

--- js/script.js
+++ js/script.js
@@ -27,3 +27,28 @@ const trendingDrinks = [
     "Aojiru",
     "Mugicha",
 ]
+
+const cards = [
+    {
+        imgSrc: sushi12,
+        alt: "sushi-12",
+        title: "Chezu Sushi",
+        rating: "4.8",
+        price: "$21.00"
+    },
+    {
+        imgSrc: sushi11,
+        alt: "sushi-11",
+        title: "Originale Sushi",
+        rating: "4.8",
+        price: "$21.00",
+        active: true
+    },
+    {
+        imgSrc: sushi10,
+        alt: "sushi-10",
+        title: "Ramen Legendo",
+        rating: "4.8",
+        price: "$21.00"
+    }
+];
