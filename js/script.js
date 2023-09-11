1f801f4c3e66cedf85389d2ee8370e100ea4212d
kooya3 <elyeestatua@gmail.com>
2023-09-11 20:32:15 +0300

    ops(@JS): AOS animation setting

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ import sushi12 from '../assets/sushi-12.png'
 import sushi11 from '../assets/sushi-11.png'
 import sushi10 from '../assets/sushi-10.png'
 
-
-import AOS from "aos";
-import "aos/dist/aos.css";
+// init AOS animation
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
