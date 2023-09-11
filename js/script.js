21bccbccfd82785f89ed944affe72306b47f8c9d
kooya3 <elyeestatua@gmail.com>
2023-09-11 20:29:11 +0300

    build(@JS): Importing and installing AOS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@ import star from '../assets/star.svg'
 import sushi12 from '../assets/sushi-12.png'
 import sushi11 from '../assets/sushi-11.png'
 import sushi10 from '../assets/sushi-10.png'
+
+
+import AOS from "aos";
+import "aos/dist/aos.css";
